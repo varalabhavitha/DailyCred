6d781de957115a74dc3ed0c48a41bd6e4ea2c450
rajashekhar20401-dotcom <rajashekhar20401@gmail.com>
2026-04-21 10:35:09 +0530

    deleted commented code

--- Dailycred/src/main/java/com/unqiuehire/kashflow/dto/requestdto/BorrowerRequestDto.java
+++ Dailycred/src/main/java/com/unqiuehire/kashflow/dto/requestdto/BorrowerRequestDto.java
@@ -1,28 +1,3 @@
-//package com.unqiuehire.kashflow.dto.requestdto;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class BorrowerRequestDto {
-//
-//    private String borrowerName;
-//    private String dateOfBirth;
-//    private String password;
-//    private Boolean isActive;
-//    private String phoneNumber;
-//    private String pincode;
-//    private String address;
-//    private String aadharCardNumber;
-//    private String panCardNumber;
-//}
-
-
 package com.unqiuehire.kashflow.dto.requestdto;
 
 import lombok.AllArgsConstructor;
--- Dailycred/src/main/java/com/unqiuehire/kashflow/service/BorrowerService.java
+++ Dailycred/src/main/java/com/unqiuehire/kashflow/service/BorrowerService.java
@@ -1,25 +1,3 @@
-//package com.unqiuehire.kashflow.service;
-//
-//import com.unqiuehire.kashflow.dto.requestdto.BorrowerRequestDto;
-//import com.unqiuehire.kashflow.dto.responsedto.ApiResponse;
-//import com.unqiuehire.kashflow.dto.responsedto.BorrowerResponseDto;
-//
-//import java.util.List;
-//
-//public interface BorrowerService {
-//
-//    ApiResponse<BorrowerResponseDto> createBorrower(BorrowerRequestDto borrowerRequestDto);
-//
-//    ApiResponse<BorrowerResponseDto> getBorrowerById(Long BorrowerId);
-//
-//    ApiResponse<List<BorrowerResponseDto>> getAllBorrowers();
-//
-//    ApiResponse<BorrowerResponseDto> updateBorrower(Long borrowerId, BorrowerRequestDto borrowerRequestDto);
-//
-//    ApiResponse<String> deleteBorrower(Long borrowerId);
-//}
-
-
 package com.unqiuehire.kashflow.service;
 
 import com.unqiuehire.kashflow.dto.requestdto.BorrowerRequestDto;
